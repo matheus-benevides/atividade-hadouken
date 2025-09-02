7a930ac5cd2f1c6256045d28c27251a6fe2b852c
Matheus G Benevides <matheus.g.benevides@aluno.senai.br>
2025-09-02 14:59:05 -0300

    HTML e JS prontos

---
+++ _javascript/playervideo.js
@@ -0,0 +1,40 @@
+var meuVideo = document.getElementById("player");
+
+function playPause() {
+    if (meuVideo.paused)
+        meuVideo.play();
+    else 
+        meuVideo.pause();
+}
+
+function makeBig() {
+    meuVideo.width = 700;
+}
+
+function makeSmall() {
+    meuVideo.width = 300;
+}
+
+function makeNormal() {
+    meuVideo.width = 450;
+}
+
+function telaCheia() {
+    meuVideo.requestFullscreen();
+}
+
+function aumentarVolume() {
+    meuVideo.volume += 0.1;
+}
+
+function diminuirVolume() {
+    meuVideo.volume -= 0.1;
+}
+
+function mutar() {
+    meuVideo.muted = true;
+}
+
+function desmutar() {
+    meuVideo.muted = false;
+}
